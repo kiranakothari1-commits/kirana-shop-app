53fd335ee9cd86e5b5921c964f771c0728e2e624
Prince Kothari <princekothari9102007@gmail.com>
2025-10-28 05:25:26 +0530

    Use environment variables for Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Ensure environment variables exist
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://bjznunoyfhwuevhylnxa.supabase.co';
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqem51bm95Zmh3dWV2aHlsbnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0Mjc0MjcsImV4cCI6MjA3NzAwMzQyN30.nmCzTTemHUUDpTlJEhxWJLBZJrLh-qrF0K_HggDr6QM';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase environment variables');
+  throw new Error('Missing Supabase environment variables!');
 }
 
 const supabase = createClient(supabaseUrl, supabaseKey, {
